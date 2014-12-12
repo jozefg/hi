59ecdb7f1f6fb6db201dabb4dfaaff5b553151ad
Danny Gratzer <jozefg@cmu.edu>
2014-12-11 23:06:34 -0500

    Added haskell-src-exts + a basic parsing function

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,20 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
+import Language.Haskell.Exts
+import System.Environment
+import System.Exit
+
+runParser :: FilePath -> IO Module
+runParser f =
+  parseFile f >>= \case
+    ParseOk a -> return a
+    ParseFailed loc err -> do
+      putStrLn ("Parsing failure: " ++ prettyPrint loc)
+      putStrLn err
+      exitFailure
 
 main :: IO ()
-main = putStrLn "Write Yourself a Haskell"
+main = do
+  [f] <- getArgs
+  _ <- runParser f
+  putStrLn "Parsed OK"
