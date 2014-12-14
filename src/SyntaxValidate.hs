ad2243f19ddabc64cf5942e61f54ac3f52fb89bb
Danny Gratzer <jozefg@cmu.edu>
2014-12-14 15:51:57 -0500

    Cleaned up SyntaxValidate to use Source.hs

--- src/SyntaxValidate.hs
+++ src/SyntaxValidate.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE LambdaCase #-}
 module SyntaxValidate (InvalidConstruct, syntaxValidate) where
-import Language.Haskell.Exts.Syntax
+import           Language.Haskell.Exts.Syntax
+import qualified Source                       as S
 
 data InvalidConstruct = InvalidConstruct SrcLoc String
+type Validate = Either InvalidConstruct
 
 notSup :: SrcLoc -> String -> Either InvalidConstruct a
 notSup loc str = Left (InvalidConstruct loc str)
@@ -11,48 +13,47 @@ instance Show InvalidConstruct where
   show (InvalidConstruct loc message) =
     "Invalid construct at " ++ show loc ++ "\t" ++ message
 
-tyValidate :: Type -> Either InvalidConstruct ()
+tyValidate :: Type -> Validate S.Type
 tyValidate = undefined
 
-cxtValidate :: Context -> Either InvalidConstruct ()
+cxtValidate :: Context -> Validate S.Cxt
 cxtValidate = undefined
 
-classValidate :: ClassDecl -> Either InvalidConstruct ()
+classValidate :: ClassDecl -> Validate (S.Decl SrcLoc)
 classValidate = undefined
 
-instValidate :: InstDecl -> Either InvalidConstruct ()
+instValidate :: InstDecl -> Validate (S.Decl SrcLoc)
 instValidate = undefined
 
-matchValidate :: Match -> Either InvalidConstruct ()
+matchValidate :: Match -> Validate (S.Match SrcLoc)
 matchValidate = undefined
 
-constrValidate :: QualConDecl -> Either InvalidConstruct ()
+constrValidate :: QualConDecl -> Validate S.Constr
 constrValidate = undefined
 
-declValidate :: Decl -> Either InvalidConstruct ()
+declValidate :: Decl -> Validate (S.Decl SrcLoc)
 declValidate = \case
-  TypeDecl _ _ _ t -> tyValidate t
-  TypeFamDecl loc _ _ _ -> notSup loc "Type families"
-  ClosedTypeFamDecl loc _ _ _ _ -> notSup loc "Closed type families"
-  DataDecl _ _ [] _ _ constrs _ -> mapM_ constrValidate constrs
+  TypeDecl _ _ _ t -> undefined
+  DataDecl _ _ [] _ _ constrs _ -> undefined
+  ClassDecl _ cxt _ [_] [] cls -> undefined
+  InstDecl _ Nothing _ cxt _ tys inst -> undefined
+  InfixDecl _ _ _ _ -> undefined
+  TypeSig _ _ t -> undefined
+  FunBind ms -> undefined
+  ClassDecl loc _ _ _ _ _ -> notSup loc "Type class extensions"
+  InstDecl loc _ _ _ _ _ _ ->
+    notSup loc "Unsupported instance declaration extensions"
+  DerivDecl loc _ _ _ _ _ -> notSup loc "Deriving declarations"
   DataDecl loc _ _ _ _ _ _ -> notSup loc "Unsupported data type extensions"
   GDataDecl loc _ _ _ _ _ _ _ -> notSup loc "GADT"
   DataFamDecl loc _ _ _ _ -> notSup loc "Data families"
   TypeInsDecl loc _ _ -> notSup loc "Type family Instance"
   DataInsDecl loc _ _ _ _ -> notSup loc "Type family instance"
   GDataInsDecl loc _ _ _ _ _ -> notSup loc "Type family instance"
-  ClassDecl _ cxt _ [_] [] cls -> cxtValidate cxt >> mapM_ classValidate cls
-  ClassDecl loc _ _ _ _ _ -> notSup loc "Type class extensions"
-  InstDecl _ Nothing _ cxt _ tys inst ->
-    mapM_ tyValidate tys >> cxtValidate cxt >> mapM_ instValidate inst
-  InstDecl loc _ _ _ _ _ _ ->
-    notSup loc "Unsupported instance declaration extensions"
-  DerivDecl loc _ _ _ _ _ -> notSup loc "Deriving declarations"
-  InfixDecl _ _ _ _ -> Right ()
   DefaultDecl loc _ -> notSup loc "Default declarations"
   SpliceDecl loc _ -> notSup loc "Splices"
-  TypeSig _ _ t -> tyValidate t
-  FunBind ms -> mapM_ matchValidate ms
+  TypeFamDecl loc _ _ _ -> notSup loc "Type families"
+  ClosedTypeFamDecl loc _ _ _ _ -> notSup loc "Closed type families"
   PatBind loc _ _ _ -> notSup loc "Pattern bindings, this seems hard"
   ForImp loc _ _ _ _ _ -> notSup loc "FFI"
   ForExp loc _ _ _ _ -> notSup loc "FFI"
@@ -67,8 +68,8 @@ declValidate = \case
   AnnPragma loc _ -> notSup loc "Pragma"
   MinimalPragma loc _ -> notSup loc "Pragma"
 
-syntaxValidate :: Module -> Either InvalidConstruct ()
+syntaxValidate :: Module -> Validate [S.Decl SrcLoc]
 syntaxValidate (Module loc _ (_:_) _ _ _ _) = notSup loc "Pragmas"
 syntaxValidate (Module loc _ _ _ (Just (_:_)) _ _) = notSup loc "Export lists"
 syntaxValidate (Module loc _ _ _ _ (_:_) _) = notSup loc "Imports"
-syntaxValidate (Module _ _ _ _ _ _ decs) = mapM_ declValidate decs
+syntaxValidate (Module _ _ _ _ _ _ decs) = mapM declValidate decs
