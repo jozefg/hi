98d3a1983ef778ccbd081114a1e2300219f73870
Danny Gratzer <jozefg@cmu.edu>
2014-12-14 10:55:59 -0500

    Started fleshing out wyah's AST

---
+++ src/Source.hs
@@ -0,0 +1,30 @@
+module Source where
+
+data Name a = Name a String
+            deriving (Eq, Show, Ord)
+
+data Kind = Star | KFun Kind Kind
+
+data PolyType = Forall (Name ()) Type
+data Type = TFun Type Type
+          | TTuple Type Type
+          | TUnit
+          | TVar (Name ())
+          | TCon (Name ())
+          | TApp Type Type
+          | TList Type
+          | TInt
+          | TDouble
+          | TBool
+          | TIO
+          | TChar
+
+data Constr a
+
+data Pat a
+data Match a
+
+data Cxt a
+data Class a
+data Decl a
+data Expr a
