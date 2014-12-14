95c80502b5c32961ce5535858122a9d9483a590c
Danny Gratzer <jozefg@cmu.edu>
2014-12-14 11:08:15 -0500

    Added most expressions to AST

--- src/Source.hs
+++ src/Source.hs
@@ -1,16 +1,16 @@
 module Source where
 
-data Name a = Name a String
+data Name = Name String
             deriving (Eq, Show, Ord)
 
 data Kind = Star | KFun Kind Kind
 
-data PolyType = Forall [Name ()] Type
+data PolyType = Forall [Name] Type
 data Type = TFun Type Type
           | TTuple Type Type
           | TUnit
-          | TVar (Name ())
-          | TCon (Name ())
+          | TVar Name
+          | TCon Name
           | TApp Type Type
           | TList Type
           | TInt
@@ -27,4 +27,21 @@ data Match a
 data Cxt a
 data Class a
 data Decl a
-data Expr a
+
+data Binding a
+
+data Exp a = Var a Name
+           | Con a Name
+           | LitChar a Char
+           | LitInt a Int
+           | LitString a String
+           | App a (Exp a) (Exp a)
+           | InfixApp a (Exp a) (Exp a) (Exp a) -- Operator in the middle
+           | LeftSection a (Exp a) (Exp a) -- Operator on the right
+           | RightSection a (Exp a) (Exp a) -- Operator on the left
+           | List a [Exp a]
+           | Let a [Binding a] (Exp a)
+           | If (Exp a) (Exp a) (Exp a)
+           | Tuple (Exp a) (Exp a)
+           | Lambda a [Pat a] (Exp a)
+           | Case a (Exp a) [(Pat a, Exp a)]
